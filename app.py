a73523fcde29597f3c87e1b8b67a46197eb7e030
bilalriaz983084-tech <bilalriaz983084@gmail.com>
2026-09-03 19:39:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -147,5 +147,5 @@ def facebook_download():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
